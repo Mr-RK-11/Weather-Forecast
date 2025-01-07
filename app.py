c749751c3ba802b0638353513d42c68cd5e6a000
Mr-RK-11 <rohithkumarh1156@gmail.com>
2025-01-07 20:11:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-from flask import Flask, render_template, request, jsonify #webframework
-import requests #retieve data from API
-from collections import defaultdict #
-import webbrowser
-import time
-import threading
+from flask import Flask, render_template, request, jsonify
+import requests
+from collections import defaultdict
+import os
 
 app = Flask(__name__)
 
@@ -12,10 +10,7 @@ API_KEY = '946a915a7bba4fdb8d201b991abde4a3'
 @app.route('/')
 def home():
     return render_template('index.html')
-def open_browser():
-    # Give Flask time to start before opening the browser
-    time.sleep(1)
-    webbrowser.open("http://127.0.0.1:5000")
+
 @app.route('/get_weather', methods=['POST'])
 def get_weather():
     city = request.form.get('city')
@@ -23,11 +18,10 @@ def get_weather():
         url = f'http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={API_KEY}&units=metric'
         response = requests.get(url).json()
 
-        if response['cod'] == "200":  
+        if response['cod'] == "200":
             grouped_weather = defaultdict(list)
-
             for item in response['list']:
-                date = item['dt_txt'].split(' ')[0]  # Extract date only (YYYY-MM-DD)
+                date = item['dt_txt'].split(' ')[0]
                 data = {
                     'date_time': item['dt_txt'],
                     'temperature': round(item['main']['temp']),
@@ -37,15 +31,13 @@ def get_weather():
                     'icon': item['weather'][0]['icon']
                 }
                 grouped_weather[date].append(data)
-
-            return jsonify(dict(grouped_weather))  # Convert to regular dict
+            return jsonify(dict(grouped_weather))
         else:
             return jsonify({'error': 'City not found'})
     return jsonify({'error': 'No city provided'})
 
 if __name__ == '__main__':
-    # Start the browser opening in a separate thread
-    threading.Thread(target=open_browser).start()
+    # Only open browser in development
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
     
-    # Run the Flask web server
-    app.run(debug=True, use_reloader=False)
